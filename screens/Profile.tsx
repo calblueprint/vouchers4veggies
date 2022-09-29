1e55b0b7f32c02437f0b140a5fb7711ccddecc2b
Selene Huang <selene.huang@gmail.com>
2022-09-28 22:10:29 -0700

    Updated profile

--- screens/Profile.tsx
+++ screens/Profile.tsx
@@ -1,9 +1,15 @@
 import { StyleSheet, Text, View } from 'react-native';
+import ProfilePicture from '../components/ProfilePicture'
 
 export default function Profile () {
   return (
     <View style={styles.container}>
-      <Text>Edit Profile</Text>
+      <Text style={styles.row}>Edit Profile</Text>
+      <View style={styles.row}>
+      <ProfilePicture/>
+      <Text>Name</Text>
+      </View>
+      <Text style={styles.row}>Uhhhh pfp data goes here :(</Text>
     </View>
   );
 }
@@ -15,4 +21,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  row: {
+    flex: 1,
+    flexDirection: 'row',
+  }
 });
