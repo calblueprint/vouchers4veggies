c7af51e0623c8c96b539327bdc0e12880ac3b309
Allison Hong <allisonhong1023@berkeley.edu>
2022-09-28 20:25:22 -0700

    navbar in progress:

--- App.tsx
+++ App.tsx
@@ -1,12 +1,32 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StyleSheet, Text, View } from 'react-native';
 
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Vouchers 4 Veggies</Text>
-    </View>
+    <NavigationContainer>
+      <View style={styles.container}>
+        <Text>Vouchers 4 Veggies</Text>
+      </View>
+    </NavigationContainer>
   );
+}
+
+const Stack = createNativeStackNavigator(); 
+
+const MyStack = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: "Welcome"}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
 }
 
 const styles = StyleSheet.create({
