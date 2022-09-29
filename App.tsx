3c3283f596f6b1304e482b78f99968a2c7a45201
Allison Hong <allisonhong1023@berkeley.edu>
2022-09-28 21:35:47 -0700

    navbar done

--- App.tsx
+++ App.tsx
@@ -5,16 +5,15 @@ import { StyleSheet, Text, View } from 'react-native';
 import Homepage from './screens/Homepage'
 import Invoice from './screens/Invoice'
 import Profile from './screens/Profile'
+import NavigationBar from './components/navbar'
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+
+const Tab = createBottomTabNavigator();
 
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <View style={styles.container}>
-        <Text>Vouchers 4 Veggies</Text>
-        <Homepage></Homepage>
-      </View>
-    </NavigationContainer>
+    <NavigationBar></NavigationBar>
   );
 }
 
---
+++ components/navbar.tsx
@@ -0,0 +1,28 @@
+import * as React from 'react';
+import {View, Text} from 'react-native';
+
+import { NavigationContainer } from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+
+import Homepage from '.././screens/Homepage'
+import Invoice from '.././screens/Invoice'
+import Profile from '.././screens/Profile'
+
+const home = "Home";
+
+const Tab = createBottomTabNavigator();
+
+export default function NavigationBar() {
+    return (
+        <NavigationContainer>
+            <Tab.Navigator
+                initialRouteName={home}
+                >
+                <Tab.Screen name="Home" component={Homepage}></Tab.Screen>
+                <Tab.Screen name="Invoices" component={Invoice}></Tab.Screen>
+                <Tab.Screen name="Profile" component={Profile}></Tab.Screen>
+            </Tab.Navigator>
+        </NavigationContainer>
+    );
+}
+
