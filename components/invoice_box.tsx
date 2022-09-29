258f544168a497838aec54c74ce6bc900aa39c0f
Allison Hong <allisonhong1023@berkeley.edu>
2022-09-28 21:58:57 -0700

    invoice screen ip

---
+++ components/invoice_box.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import {View, Text} from 'react-native';
+
+const InvoiceBox = (props: string) => {
+    return (
+        <div
+            >
+
+        </div>
+    )
+}
