3983b9f6ded1e5891219151fa166679a38c04211
Sauhard Jain <sauhard@berkeley.edu>
2022-10-02 15:18:01 -0700

    Revert "repo-setup"
    This reverts commit 9faf5c3a1e4515f007588e3c7d92977e5dc191cd.

--- App.tsx
+++ App.tsx
@@ -1,8 +1,7 @@
 import { StyleSheet, Text, View } from 'react-native';
-import { getAllTestDocs } from './database/queries';
+
 
 export default function App() {
-  getAllTestDocs();
   return (
     <View style={styles.container}>
       <Text>Vouchers 4 Veggies</Text>
