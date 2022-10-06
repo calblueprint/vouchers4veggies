c8851bcf971d134ffc4ff64c6c8f6c9ede1b1b90
Selene Huang <selene.huang@gmail.com>
2022-10-06 11:58:30 -0700

    app screen updated for testing

--- App.tsx
+++ App.tsx
@@ -2,12 +2,8 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StyleSheet, Text, View } from 'react-native';
-import Homepage from './screens/Homepage'
-import Invoice from './screens/Invoice'
-import Profile from './screens/Profile'
-import Login from './screens/Login'
-import NavigationBar from './components/Navbar'
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Login from './screens/Login';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 const Tab = createBottomTabNavigator();
 
@@ -17,26 +13,7 @@ import { getAllTestDocs } from './src/database/queries';
 export default function App() {
   //call the function here
   getAllTestDocs();
-  return (
-    
-    <Login></Login>
-  );
-}
-
-const Stack = createNativeStackNavigator(); 
-
-const MyStack = () => {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Home"
-          component={Homepage}
-        />
-        <Stack.Screen name="Profile" component={Profile} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  )
+  return <Login></Login>;
 }
 
 const styles = StyleSheet.create({
