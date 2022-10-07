1b5732d1c6358adae32d6dcbf44e1b21ac9b6af3
Ethan Auyeung <ethanauyeung@berkeley.edu>
2022-10-06 18:12:13 -0700

    Added color and font setup

---
+++ assets/Fonts.tsx
@@ -0,0 +1,54 @@
+import styled from 'styled-components/native';
+
+export const H1Heading = styled.Text`
+	font-family: 'Manrope';
+	font-style: normal;
+	font-weight: 700;
+	font-size: 36px;
+	line-height: 49px;
+`;
+
+export const H2Heading = styled.Text`
+	font-family: 'Manrope';
+	font-style: normal;
+	font-weight: 700;
+	font-size: 30px;
+	line-height: 41px;
+`;
+
+export const H3_Subheading = styled.Text`
+	font-family: 'Manrope';
+	font-style: normal;
+	font-weight: 500;
+	font-size: 24px;
+	line-height: 33px;;
+`;
+
+export const H4_Card_Nav_Tab = styled.Text`
+	font-family: 'Manrope';
+	font-style: normal;
+	font-weight: 500;
+	font-size: 18px;
+	line-height: 25px;
+`;
+
+export const Body_1_Text = styled.Text`
+	font-family: 'Manrope';
+	font-style: normal;
+	font-weight: 400;
+	font-size: 14px;
+	line-height: 19px;	
+`;
+
+export const Body_2_Subtext = styled.Text`
+	font-family: 'Manrope';
+	font-style: normal;
+	font-weight: 400;
+	font-size: 10px;
+	line-height: 14px;	
+`;
+
+
+
+
+
