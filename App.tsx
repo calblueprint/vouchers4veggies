1b83205a376c67632736c37ca9d2184db108a5f5
Selene Huang <selene.huang@gmail.com>
2022-10-09 15:03:28 -0700

    Updated functions to arrow functions

--- App.tsx
+++ App.tsx
@@ -2,8 +2,8 @@ import * as Font from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
 import React, { useCallback, useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import Login from './screens/Login';
-import StartScreen from './screens/StartScreen';
+import { Login } from './screens/Login';
+import { StartScreen } from './screens/StartScreen';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 //add this import statement
