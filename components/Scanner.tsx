042d14b0451d2b3107e28864c47e1332a6ccc609
Selene Huang <selene.huang@gmail.com>
2022-10-16 14:39:17 -0700

    homepage code from allison-and-selene/screens

---
+++ components/Scanner.tsx
@@ -0,0 +1,10 @@
+import react from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function Scanner() {
+  return (
+    <View style={{ backgroundColor: '#000000', padding: '10px' }}>
+      <Text style={{ color: '#FFFFFF' }}>scanner coming soon (she lied)</Text>
+    </View>
+  );
+}
