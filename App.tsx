56e35a50c0617bf25623ccf6ce04d28e77155a25
Selene Huang <selene.huang@gmail.com>
2022-10-16 16:19:07 -0700

    rebasing

--- App.tsx
+++ App.tsx
@@ -2,11 +2,15 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StyleSheet, Text, View } from 'react-native';
+<<<<<<< HEAD
 import Homepage from './screens/Homepage';
 import Invoice from './screens/Invoice';
 import Profile from './screens/Profile';
 import Login from './screens/Login';
 import NavigationBar from './components/Navbar';
+=======
+import Login from './screens/Login';
+>>>>>>> c8851bc (app screen updated for testing)
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 const Tab = createBottomTabNavigator();
@@ -17,21 +21,10 @@ import { getAllTestDocs } from './src/database/queries';
 import { H1Heading } from './assets/Fonts';
 
 export default function App() {
+  //call the function here
+  getAllTestDocs();
   return <Login></Login>;
 }
-
-const Stack = createNativeStackNavigator();
-
-const MyStack = () => {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={Homepage} />
-        <Stack.Screen name="Profile" component={Profile} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-};
 
 const styles = StyleSheet.create({
   container: {
