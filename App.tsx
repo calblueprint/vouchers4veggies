5b18bb59d2744868d9d641b5ec9651a9c58ccfe1
Selene Huang <selene.huang@gmail.com>
2022-10-16 16:20:56 -0700

    rebasing complete

--- App.tsx
+++ App.tsx
@@ -1,6 +1,4 @@
 import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StyleSheet } from 'react-native';
 import { Login } from './screens/LoginScreen';
 import { StartScreen } from './screens/StartScreen';
@@ -8,23 +6,10 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 const Tab = createBottomTabNavigator();
 
-import VendorsListDemo from './VendorsListDemo';
-//add this import statement
 import { getAllTestDocs } from './src/database/queries';
 import { H1Heading } from './assets/Fonts';
 
 export default function App() {
-  const Stack = createNativeStackNavigator();
-
-  <NavigationContainer>
-    <Stack.Navigator initialRouteName="Start">
-      <Stack.Screen name="Start" component={StartScreen} />
-      <Stack.Screen name="Login" component={Login} />
-      <Stack.Screen name="Signup" component={Login} /> {/* temporary fix */}
-    </Stack.Navigator>
-  </NavigationContainer>;
-  getAllTestDocs();
-
   // return <StartScreen></StartScreen>;
   return <Login></Login>;
 }
