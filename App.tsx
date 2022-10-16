71f16e5dc4db25d42bcaf3c3a451c38f7fdc2400
Selene Huang <selene.huang@gmail.com>
2022-10-16 16:15:41 -0700

    rebasing continues

--- App.tsx
+++ App.tsx
@@ -1,3 +1,6 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StyleSheet, Text, View } from 'react-native';
 import Homepage from './screens/Homepage'
 import Invoice from './screens/Invoice'
