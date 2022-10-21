6eaa2764b37c69bf188149edc8d040cc978168b3
Annie Wang <wangannie@berkeley.edu>
2022-10-20 20:38:50 -0700

    [fonts][fix] Load fonts on app entry with splash screen (#9)
    * [feat] await loading async fonts on app load
    
    * [fix] update font styles to use correct corresponding font-families

--- App.tsx
+++ App.tsx
@@ -1,15 +1,64 @@
-import { StyleSheet, Text, View } from 'react-native';
-
-import VendorsListDemo from './VendorsListDemo';
-//add this import statement
-import { getAllTestDocs } from './src/database/queries';
+import * as Font from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import React, { useCallback, useEffect, useState } from 'react';
+import { StyleSheet, View } from 'react-native';
 import { H1Heading } from './assets/Fonts';
+import { getAllTestDocs } from './src/database/queries';
+import VendorsListDemo from './VendorsListDemo';
+
+// Keep the splash screen visible while we fetch resources
+SplashScreen.preventAutoHideAsync();
+
+async function loadResourcesAsync() {
+  await Promise.all([
+    // Pre-load other resources (i.e. images) here
+    Font.loadAsync({
+      'manrope-bold': require('./assets/Manrope/static/Manrope-Bold.ttf'),
+      'manrope-extraBold': require('./assets/Manrope/static/Manrope-ExtraBold.ttf'),
+      'manrope-extraLight': require('./assets/Manrope/static/Manrope-ExtraLight.ttf'),
+      'manrope-light': require('./assets/Manrope/static/Manrope-Light.ttf'),
+      'manrope-medium': require('./assets/Manrope/static/Manrope-Medium.ttf'),
+      'manrope-regular': require('./assets/Manrope/static/Manrope-Regular.ttf'),
+      'manrope-semiBold': require('./assets/Manrope/static/Manrope-SemiBold.ttf'),
+    }),
+  ]);
+}
 
 export default function App() {
-  //call the function here
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await loadResourcesAsync();
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        // Tell the application to render
+        setAppIsReady(true);
+      }
+    }
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      // This tells the splash screen to hide immediately! If we call this after
+      // `setAppIsReady`, then we may see a blank screen while the app is
+      // loading its initial state and rendering its first pixels. So instead,
+      // we hide the splash screen once we know the root view has already
+      // performed layout.
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+  }
+
   getAllTestDocs();
   return (
-    <View style={styles.container}>
+    <View style={styles.container} onLayout={onLayoutRootView}>
       <H1Heading>Vouchers 4 Veggies</H1Heading>
       <VendorsListDemo />
     </View>
