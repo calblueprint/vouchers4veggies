84199e308ec598ecdbe582e9dff926a400548281
Selene Huang <selene.huang@gmail.com>
2022-10-22 11:52:23 -0700

    moved screens and components to src

--- App.tsx
+++ App.tsx
@@ -2,10 +2,10 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StyleSheet, Text, View } from 'react-native';
-import { Homepage } from './screens/Homepage';
-import { Invoice } from './screens/Invoice';
-import { Profile } from './screens/Profile';
-import { NavigationBar } from './components/NavigationBar';
+import { Homepage } from './src/screens/Homepage';
+import { Invoice } from './src/screens/Invoice';
+import { Profile } from './src/screens/Profile';
+import { NavigationBar } from './src/components/NavigationBar';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 const Tab = createBottomTabNavigator();
