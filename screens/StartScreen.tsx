860aa9b38358dd4bcee67809b8891ce7260051fa
Selene Huang <selene.huang@gmail.com>
2022-10-22 15:46:34 -0700

    rebasing

--- screens/StartScreen.tsx
+++ screens/StartScreen.tsx
@@ -1,24 +1,49 @@
+<<<<<<< HEAD
 import { StyleSheet, Text, View, Button } from 'react-native';
 import React from 'react';
 
 export const StartScreen = () => {
   const toLoginScreen = () => {};
   const toSignupScreen = () => {};
+=======
+import { StyleSheet, TextInput, Text, View, Button } from 'react-native';
+import { useState } from 'react';
+
+export default function StartScreen(props) {
+  const toLoginScreen = () => {
+    props.navigation.navigate('Login');
+  };
+  const toSignupScreen = () => {
+    props.navigation.navigate('Signup');
+  };
+>>>>>>> 85d009a (setting up navigation)
 
   return (
     <View style={styles.container}>
       <Text style={styles.heading}>Hello! Let's get started.</Text>
 
+<<<<<<< HEAD
       <View style={styles.button}>
         <Button onPress={toLoginScreen} title="Login" color="#f2f2f2" />
       </View>
 
       <View style={styles.button}>
+=======
+      <View>
+        <Button onPress={toLoginScreen} title="Login" color="#f2f2f2" />
+      </View>
+
+      <View>
+>>>>>>> 85d009a (setting up navigation)
         <Button onPress={toSignupScreen} title="Signup" color="#d9d9d9" />
       </View>
     </View>
   );
+<<<<<<< HEAD
 };
+=======
+}
+>>>>>>> 85d009a (setting up navigation)
 
 const styles = StyleSheet.create({
   container: {
@@ -31,10 +56,13 @@ const styles = StyleSheet.create({
     fontSize: 36,
     fontWeight: 'bold',
     textAlign: 'center',
+<<<<<<< HEAD
     margin: '10%',
   },
   button: {
     width: '80%',
     margin: '10px',
+=======
+>>>>>>> 85d009a (setting up navigation)
   },
 });
