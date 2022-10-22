bea4ead3a05759e02a1f20a95554b9098c05b263
Selene Huang <selene.huang@gmail.com>
2022-10-22 15:36:27 -0700

    updating transaction screen

--- src/components/TransactionCard.tsx
+++ src/components/TransactionCard.tsx
@@ -1,16 +1,27 @@
 import { StyleSheet, Text, View, Button } from 'react-native';
 import React, { useState } from 'react';
-import { H2Heading } from '../../assets/Fonts';
+import { H3_Subheading, Body_2_Subtext, Body_1_Text } from '../../assets/Fonts';
+import Icon from 'react-native-vector-icons/AntDesign';
 
 export const TransactionCard = (props: any) => {
   return (
     <View style={styles.row}>
-      <View>
-        <Text>{props.id}</Text>
-        <Text>{props.date}</Text>
+      <View style={styles.left_align}>
+        <Body_2_Subtext>ID {props.id}</Body_2_Subtext>
+        <Body_1_Text>{props.date}</Body_1_Text>
       </View>
-      <Text>x{props.count}</Text>
-      <Text>${props.price}</Text>
+      <Body_1_Text style={{ ...styles.spacing, ...styles.right_align }}>
+        x{props.count}
+      </Body_1_Text>
+      <H3_Subheading style={styles.right_align_long}>
+        ${props.price}
+      </H3_Subheading>
+      <Icon.Button
+        name="right"
+        size={25}
+        style={{ ...styles.icon_button, ...styles.right_align }}
+        color="#A9A9A9"
+      />
     </View>
   );
 };
@@ -21,6 +32,38 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     flexDirection: 'row',
     alignItems: 'center',
+    justifyContent: 'flex-start',
+    borderTopStyle: 'solid',
+    borderBottomStyle: 'solid',
+    borderRadius: 2,
+    borderColor: '#f2f2f2',
+    width: '100%',
+  },
+  left_align: {
+    flex: 4,
+    flexDirection: 'column',
     justifyContent: 'center',
+    alignContent: 'flex-start',
+    paddingLeft: 20,
+  },
+  right_align: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'flex-end',
+    justifyContent: 'flex-end',
+    paddingRight: 10,
+  },
+  right_align_long: {
+    flex: 2,
+    flexDirection: 'row',
+    alignItems: 'flex-end',
+    justifyContent: 'flex-end',
+    paddingRight: 10,
+  },
+  icon_button: {
+    backgroundColor: '#fff',
+  },
+  spacing: {
+    marginRight: 30,
   },
 });
--- src/screens/TransactionsScreen.tsx
+++ src/screens/TransactionsScreen.tsx
@@ -1,11 +1,35 @@
 import React, { useState, useEffect } from 'react';
 import { Text, View, StyleSheet, Button } from 'react-native';
 import { TransactionCard } from '../components/TransactionCard';
+import { H2Heading } from '../../assets/Fonts';
+// import {} from '@expo/vector-icons';
 
 export const TransactionsScreen = () => {
+  const [transactions, setTransactions] = useState([
+    { id: '123123', date: '10/22/22 8:29AM PST', count: 4, price: 10.43 },
+    { id: '473844', date: '10/19/22 3:12PM PST', count: 10, price: 37.21 },
+    { id: '123827', date: '10/10/22 4:55AM PST', count: 2, price: 5.42 },
+    { id: '234724', date: '10/07/22 2:49AM PST', count: 1, price: 11.22 },
+    { id: '198343', date: '10/05/22 6:52PM PST', count: 12, price: 3.24 },
+    { id: '127428', date: '10/01/22 1:29PM PST', count: 3, price: 7.29 },
+  ]);
+
   return (
     <View style={styles.container}>
-      <TransactionCard id="123123" date="10/22/22" count="4" price="10.43" />
+      <View style={styles.align_right}>
+        <Button title="Add manually" />
+      </View>
+      <H2Heading>Transactions</H2Heading>
+      {/* <SearchBar></SearchBar> */}
+
+      {transactions.map(item => (
+        <TransactionCard
+          id={item.id}
+          date={item.date}
+          count={item.count}
+          price={item.price}
+        />
+      ))}
     </View>
   );
 };
@@ -15,5 +39,13 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: 'column',
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  align_right: {
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    marginTop: 20,
+    marginRight: 60,
   },
 });
