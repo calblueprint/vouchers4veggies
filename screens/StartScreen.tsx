a6982c217253eb1044bea1800f404b893f31c275
Selene Huang <selene.huang@gmail.com>
2022-10-22 15:46:41 -0700

    rebasing

--- screens/StartScreen.tsx
+++ screens/StartScreen.tsx
@@ -8,7 +8,12 @@ export const StartScreen = () => {
 =======
 import { StyleSheet, TextInput, Text, View, Button } from 'react-native';
 import { useState } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Login from './Login';
 
+<<<<<<< HEAD
 export default function StartScreen(props) {
   const toLoginScreen = () => {
     props.navigation.navigate('Login');
@@ -17,6 +22,10 @@ export default function StartScreen(props) {
     props.navigation.navigate('Signup');
   };
 >>>>>>> 85d009a (setting up navigation)
+=======
+export default function StartScreen({ navigation }) {
+  console.log(navigation);
+>>>>>>> 2f4bd8f (nonfunctional routing)
 
   return (
     <View style={styles.container}>
@@ -30,12 +39,24 @@ export default function StartScreen(props) {
       <View style={styles.button}>
 =======
       <View>
-        <Button onPress={toLoginScreen} title="Login" color="#f2f2f2" />
+        <Button
+          onPress={() => navigation.navigate('Login')}
+          title="Login"
+          color="#f2f2f2"
+        />
       </View>
 
       <View>
+<<<<<<< HEAD
 >>>>>>> 85d009a (setting up navigation)
         <Button onPress={toSignupScreen} title="Signup" color="#d9d9d9" />
+=======
+        <Button
+          onPress={() => navigation.navigate('Signup')}
+          title="Signup"
+          color="#d9d9d9"
+        />
+>>>>>>> 2f4bd8f (nonfunctional routing)
       </View>
     </View>
   );
