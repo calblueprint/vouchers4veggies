c2ff8de472e59820698c3200e308097cdcdda7c8
selene-huang <57937407+selene-huang@users.noreply.github.com>
2022-11-03 21:53:00 -0700

    Add Transaction component and placeholder Transactions screen (#13)
    * package issues
    
    * fixed package issues (i think)
    
    * yay
    
    * updating transaction screen
    
    * oops
    
    * nicened formatting
    
    * now with better spacing <3
    
    * updated imports and app.tsx
    
    * restored package-lock.json
    
    * standardized colors
    
    * Updated transaction card styling
    
    * fixed style sheet mix up
    
    * updated transaction screen styling
    
    * reverted app.tsx
    
    * updated imports
    
    * renamed styled components
    
    Co-authored-by: Noah Hernandez <noahh_@berkeley.edu>

---
+++ src/screens/Transactions/styles.ts
@@ -0,0 +1,32 @@
+import styled from 'styled-components/native';
+
+export const TransactionsContainer = styled.View`
+  flex: 1;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const FilterRow = styled.View`
+  width: 100%;
+  flex-direction: row;
+`;
+
+export const Title = styled.View`
+  padding-top: 20px;
+  padding-bottom: 20px;
+`;
+
+export const HorizontalSpacingContainer = styled.View`
+  padding-left: 15px;
+  padding-right: 15px;
+`;
+
+export const AddManuallyContainer = styled.View`
+  width: 100%;
+  flex-direction: row;
+  padding-top: 40px;
+  justify-content: flex-end;
+  margin-top: 20px;
+  margin-right: 60px;
+`;
