9738c457a2e58b5c5247d1479086268522400c9a
EthanAuyeung <ethana2442@gmail.com>
2022-11-10 15:53:15 -0800

    Fixes for PR

--- assets/TypeFieldComponents.tsx
+++ assets/TypeFieldComponents.tsx
@@ -1,42 +1,16 @@
 import styled from 'styled-components/native';
-import React from 'react';
-import { SafeAreaView, TextInput } from 'react-native';
-
-const textInputs = () => {
-  const [text, onChangeText] = React.useState('Default Text');
-  const [number, onChangeNumber] = React.useState(null);
-
-  return (
-    <SafeAreaView>
-      <TextInput
-        style={[
-          defaultBox.input,
-          typing.input,
-          entered.input,
-          errorEmpty.input,
-          errorEntered.input,
-        ]}
-        onChangeText={onChangeText}
-        value={text}
-      />
-    </SafeAreaView>
-  );
-};
 
 /**
  * Styling for default text box
  */
-export const defaultBox = styled.Text`
+export const defaultBox = styled.TextInput`
   box-sizing: border-box;
-
   position: absolute;
   left: 0%;
   right: 0%;
   top: 0%;
   bottom: 0%;
-
   background: lightGray;
-
   border: 1px solid midGray;
   border-radius: 5px;
 `;
@@ -44,17 +18,14 @@ export const defaultBox = styled.Text`
 /**
  * Styling for typing text box
  */
-export const typing = styled.Text`
+export const typing = styled.TextInput`
   box-sizing: border-box;
-
   position: absolute;
   left: 0%;
   right: 0%;
   top: 0%;
   bottom: 0%;
-
   background: offWhite;
-
   border: 1px solid magenta;
   border-radius: 5px;
 `;
@@ -62,17 +33,14 @@ export const typing = styled.Text`
 /**
  * Styling for text entered text box
  */
-export const entered = styled.Text`
+export const entered = styled.TextInput`
   box-sizing: border-box;
-
   position: absolute;
   left: 0%;
   right: 0%;
   top: 0%;
   bottom: 0%;
-
   background: offWhite;
-
   border: 1px solid midGray;
   border-radius: 5px;
 `;
@@ -80,17 +48,14 @@ export const entered = styled.Text`
 /**
  * Styling for error if empty text box
  */
-export const errorEmpty = styled.Text`
+export const errorEmpty = styled.TextInput`
   box-sizing: border-box;
-
   position: absolute;
   left: 0%;
   right: 0%;
   top: 0%;
   bottom: 0%;
-
   background: alertLightRed;
-
   border: 1px solid midGray;
   border-radius: 5px;
 `;
@@ -98,20 +63,15 @@ export const errorEmpty = styled.Text`
 /**
  * Styling for error with entered input text box
  */
-export const errorEntered = styled.Text`
+export const errorEntered = styled.TextInput`
   box-sizing: border-box;
-
   position: absolute;
   left: 0%;
   right: 0%;
   top: 0%;
   bottom: 0%;
-
   background: alertLightRed;
   /* Mid Gray */
-
   border: 1px solid midGray;
   border-radius: 5px;
 `;
-
-export default textInputs;
