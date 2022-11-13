25f645deef8aabaa28908f335eaa1eec89866ee9
Annie Wang <wangannie@berkeley.edu>
2022-11-13 15:45:25 -0800

    [cleanup] update renamed func name in comment

--- App.tsx
+++ App.tsx
@@ -45,7 +45,7 @@ export default function App() {
   const onLayoutRootView = useCallback(async () => {
     if (resourcesLoaded) {
       // This tells the splash screen to hide immediately! If we call this after
-      // `setAppIsReady`, then we may see a blank screen while the app is
+      // `setResourcesLoaded`, then we may see a blank screen while the app is
       // loading its initial state and rendering its first pixels. So instead,
       // we hide the splash screen once we know the root view has already
       // performed layout.
