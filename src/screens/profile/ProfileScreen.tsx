bc12f9cf6e80cf878518deb20f0a4c750f33f954
Annie Wang <wangannie@berkeley.edu>
2022-11-13 16:08:39 -0800

    [feat] add signout ability to profile screen

--- src/screens/profile/ProfileScreen.tsx
+++ src/screens/profile/ProfileScreen.tsx
@@ -1,12 +1,16 @@
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import React, { useContext } from 'react';
+import { Button, StyleSheet, View } from 'react-native';
 import { H2Heading, H3_Subheading } from '../../../assets/Fonts';
+import { AuthContext } from '../auth/AuthContext';
 
 export default function ProfileScreen() {
+  const { signOut } = useContext(AuthContext);
+
   return (
     <View style={styles.container}>
       <H2Heading>Profile Screen</H2Heading>
       <H3_Subheading>To be implemented...</H3_Subheading>
+      <Button onPress={signOut} title="Sign out" />
     </View>
   );
 }
