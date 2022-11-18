630a8aee0e6c5b64dda5820554c3d10f33bc3da8
Sauhard Jain <sauhardjain03@gmail.com>
2022-11-18 03:00:40 -0800

    [wip][scanning] Implement scanning screen  (#18)
    * [fix] reorganize files in /src
    
    * Add placeholder Login and Start screen (#12)
    
    * added login and start screen
    
    * Standardized colors
    
    * organize screen files
    
    * Add Transaction component and placeholder Transactions screen (#13)
    
    * package issues
    
    * fixed package issues (i think)
    
    * yay
    
    * updating transaction screen
    
    * oops
    
    * nicened formatting
    
    * now with better spacing <3
    
    * updated imports and app.tsx
    
    * restored package-lock.json
    
    * standardized colors
    
    * Updated transaction card styling
    
    * fixed style sheet mix up
    
    * updated transaction screen styling
    
    * reverted app.tsx
    
    * updated imports
    
    * renamed styled components
    
    Co-authored-by: Noah Hernandez <noahh_@berkeley.edu>
    
    * Update App.tsx
    
    * [feat] implement scanning screen
    
    * [feat] added VoucherCounter and Logo
    
    * [fix] fixed styling for scanner box
    
    * [cleanup] removed unused imports
    
    * [fix] reorganize files in /src
    
    * [feat] implement scanning screen
    
    * [feat] added VoucherCounter and Logo
    
    * [fix] fixed styling for scanner box
    
    * [cleanup] removed unused imports
    
    * add conditional rendering for review & submit button
    
    Co-authored-by: selene-huang <57937407+selene-huang@users.noreply.github.com>
    Co-authored-by: Noah Hernandez <noahh_@berkeley.edu>
    Co-authored-by: Noah Hernandez <63211322+oahnh@users.noreply.github.com>

---
+++ src/screens/scanning/styles.ts
@@ -0,0 +1,83 @@
+import { BarCodeScanner } from 'expo-barcode-scanner';
+import styled from 'styled-components/native';
+import { Colors } from '../../../assets/Colors';
+
+export const ScannerContainer = styled.View`
+  width: 277px;
+  height: 277px;
+  background: ${Colors.offWhite};
+  border: 2px solid ${Colors.lightMagenta};
+  border-radius: 12px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+  margin-bottom: 40px;
+`;
+
+export const TopContainer = styled.View`
+  width: 70%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 36px;
+`;
+
+export const LogoContainer = styled.ImageBackground`
+  width: 50px;
+  height: 60px;
+`;
+
+export const VoucherCounter = styled.View`
+  width: 30px;
+  height: 30px;
+  background: ${Colors.lightMagenta});
+  border: 1px solid ${Colors.brightMagenta};
+  border-radius: 15px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const PageContainer = styled.View`
+  margin: 5%;
+  background-color: ${Colors.offWhite};
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  margin-bottom: 125px;
+`;
+
+export const TitleContainer = styled.View`
+  padding-bottom: 12px;
+`;
+
+export const HeaderContainer = styled.View`
+  width: 277px;
+  align-items: center;
+  margin-bottom: 35px;
+`;
+
+export const ButtonMagenta = styled.TouchableOpacity`
+  text-align: center;
+  align-items: center;
+  border-width: 2px;
+  border-radius: 5px;
+  width: 277px;
+  background: ${Colors.magenta};
+  padding: 9px 9px;
+  border-color: ${Colors.magenta};
+  margin-bottom: 16px;
+`;
+
+export const Container = styled.ScrollView`
+  width: 100%;
+`;
+
+export const ButtonContainer = styled.View`
+  display: flex;
+  flex-direction: column;
+  align-content: space-between;
+`;
