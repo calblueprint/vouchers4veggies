c5225c4dbfe48655882f4c17b3e06e93ba79c468
Noah Hernandez <noahh_@berkeley.edu>
2022-11-30 01:13:58 -0800

    fixed bg color

--- src/screens/scanning/styles.ts
+++ src/screens/scanning/styles.ts
@@ -98,3 +98,7 @@ export const AggregateContainer = styled.View`
   align-items: center;
   margin-top: 10px;
 `;
+
+export const SafeArea = styled.SafeAreaView`
+  background-color: ${Colors.offWhite};
+`;
