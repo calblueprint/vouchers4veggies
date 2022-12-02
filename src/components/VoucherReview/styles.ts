ac442fd89bb340f58204046710cfc16433899726
Noah Hernandez <noahh_@berkeley.edu>
2022-12-02 02:28:01 -0800

    rebase p3 :(((

--- src/components/VoucherReview/styles.ts
+++ src/components/VoucherReview/styles.ts
@@ -2,11 +2,11 @@ import styled from 'styled-components/native';
 
 export const ReviewRow = styled.View`
   display: flex;
-  flex: 1;
   flex-direction: row;
   justify-content: flex-start;
   align-items: center;
   padding: 23px 29px;
+  width: 100%;
 `;
 
 export const DateIdContainer = styled.View`
@@ -14,7 +14,6 @@ export const DateIdContainer = styled.View`
   flex-direction: column;
   justify-content: center;
   align-items: flex-start;
-  margin-left: 29px;
 `;
 
 export const PriceEditContainer = styled.View`
