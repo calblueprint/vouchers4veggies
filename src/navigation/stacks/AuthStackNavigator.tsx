929edb542d1a6917ad88eec0ea098500d96ffff2
Noah Hernandez <noahh_@berkeley.edu>
2022-12-02 02:18:32 -0800

    fixed react imports

--- src/navigation/stacks/AuthStackNavigator.tsx
+++ src/navigation/stacks/AuthStackNavigator.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { LoginScreen } from '../../screens/auth/LoginScreen';
 import { StartScreen } from '../../screens/auth/StartScreen';
