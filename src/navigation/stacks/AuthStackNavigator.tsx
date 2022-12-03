174ad49dc3e8dc998ffecbf2cfef2ee7ff2dd1aa
Noah Hernandez <noahh_@berkeley.edu>
2022-12-02 17:10:33 -0800

    nav flow IP

--- src/navigation/stacks/AuthStackNavigator.tsx
+++ src/navigation/stacks/AuthStackNavigator.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import React from 'react';
 import LoginScreen from '../../screens/auth/LoginScreen';
 import StartScreen from '../../screens/auth/StartScreen';
 import ForgotPassword from '../../screens/auth/ForgotPasswordScreen';
