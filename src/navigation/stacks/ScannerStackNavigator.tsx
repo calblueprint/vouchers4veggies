b41abf811dc3b09e5a649dfd26189f196e175d7a
Sauhard Jain <sauhardjain03@gmail.com>
2022-12-03 14:32:24 -0800

    [feat] added navigation for scanning flow

--- src/navigation/stacks/ScannerStackNavigator.tsx
+++ src/navigation/stacks/ScannerStackNavigator.tsx
@@ -1,7 +1,7 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React from 'react';
-import ScannerScreen from '../../screens/scanner/ScannerScreen';
 import ManualVoucherScreen from '../../screens/scanning/ManualVoucherScreen';
+import ScanningScreen from '../../screens/scanning/ScanningScreen';
 import { ScannerStackParamList } from '../types';
 
 const ScannerStack = createNativeStackNavigator<ScannerStackParamList>();
@@ -16,11 +16,12 @@ export default function ScannerStackNavigator() {
         headerShown: false,
       }}
     >
-      <ScannerStack.Screen name="Scanner" component={ScannerScreen} />
+      <ScannerStack.Screen name="ScanningScreen" component={ScanningScreen} />
       <ScannerStack.Screen
-        name="ManualVoucher"
+        name="ManualVoucherScreen"
         component={ManualVoucherScreen}
       />
+      {/* <ScannerStack.Screen name="ReviewScreen" component={ReviewScreen} /> */}
     </ScannerStack.Navigator>
   );
 }
