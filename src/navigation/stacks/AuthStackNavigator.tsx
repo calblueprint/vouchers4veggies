b649dcbf68ddfe3c883b7d8cabc478bb0c684fbc
Allison Hong <allisonhong1023@berkeley.edu>
2022-12-03 14:07:44 -0800

    merging w. main

--- src/navigation/stacks/AuthStackNavigator.tsx
+++ src/navigation/stacks/AuthStackNavigator.tsx
@@ -1,10 +1,9 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React from 'react';
-import { LoginScreen } from '../../screens/auth/LoginScreen';
+import LoginScreen from '../../screens/auth/LoginScreen';
 import StartScreen from '../../screens/auth/StartScreen';
 import { SignupScreen } from '../../screens/auth/SignupScreen';
 import { CreatePassword } from '../../screens/auth/CreatePassword';
-
 
 import { AuthStackParamList } from '../types';
 
