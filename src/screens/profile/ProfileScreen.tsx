cac0f9ef99c5a308e5403dc6c21c9c16d5f0a211
EthanAuyeung <ethana2442@gmail.com>
2022-12-03 17:30:25 -0800

    Profile Screen Progress

--- src/screens/profile/ProfileScreen.tsx
+++ src/screens/profile/ProfileScreen.tsx
@@ -1,28 +1,17 @@
 import React, { useState } from 'react';
-import { Button, StyleSheet, View } from 'react-native';
+import { Button, StyleSheet, View, Text } from 'react-native';
 import { shouldUseActivityState } from 'react-native-screens';
 import {
-  H2Heading,
-  H3_Subheading,
-  H4_Card_Nav_Tab,
   H1Heading,
-  Body_1_Text,
+  Body1Text,
   H3Subheading,
+  H4CardNavTab,
+  H2Heading,
 } from '../../../assets/Fonts';
-import { H2Heading, H3Subheading } from '../../../assets/Fonts';
 import { signOut } from '../../utils/authUtils';
 import { useAuthContext } from '../auth/AuthContext';
-import { ButtonMagenta } from '../scanning/styles';
+import { ButtonMagenta, LogoContainer } from '../scanning/styles';
 import { ButtonTextWhite } from '../../../assets/Fonts';
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
 
 export default function ProfileScreen() {
   const { dispatch } = useAuthContext();
@@ -33,9 +22,79 @@ export default function ProfileScreen() {
 
   return (
     <View style={styles.container}>
-      <H2Heading>Profile Screen</H2Heading>
-      <H3Subheading>To be implemented...</H3Subheading>
-      <Button onPress={() => signOut(dispatch)} title="Sign out" />
+      {/* logo placeholder */}
+      <LogoContainer>
+        <View style={{ backgroundColor: 'black', width: 50, height: 59.29 }}>
+          <Text style={{ color: 'white' }}>{`\n  Logo`}</Text>
+        </View>
+      </LogoContainer>
+      <H1Heading style={styles.name} name={name} setName={setName}>
+        Noah Hernandez
+      </H1Heading>
+      <View style={styles.body_text1}>
+        <Body1Text>Email</Body1Text>
+        <H3Subheading email={email} setEmail={setEmail}>
+          email
+        </H3Subheading>
+      </View>
+      <View style={styles.container}>
+        <Body1Text>Phone Number</Body1Text>
+        <H3Subheading
+          style={styles.name}
+          phoneNumber={phoneNumber}
+          setPhoneNumber={setPhoneNumber}
+        >
+          (123) 456-7890
+        </H3Subheading>
+      </View>
+      <View style={styles.container}>
+        <Body1Text>Password</Body1Text>
+        <H3Subheading password={password} setPassword={setPassword}>
+          password
+        </H3Subheading>
+      </View>
+      <ButtonMagenta>
+        <ButtonTextWhite>Log Out</ButtonTextWhite>
+      </ButtonMagenta>
     </View>
   );
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  center_text: {
+    textAlign: 'center',
+  },
+  body_text1: {
+    textAlign: 'center',
+    width: 35,
+    height: 19,
+    left: 29,
+    top: 277,
+  },
+  name: {
+    textAlign: 'center',
+    width: 254,
+    height: 44,
+    left: 60,
+    top: 186,
+  },
+  phone: {
+    textAlign: 'center',
+    width: 132,
+    height: 25,
+    left: 29,
+    top: 376,
+  },
+  button: {
+    textAlign: 'center',
+    width: 277,
+    height: 43,
+    left: 49,
+    top: 615,
+  },
+});
