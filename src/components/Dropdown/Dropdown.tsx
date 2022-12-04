0255cb028494fba7a2319178a34795324b293c4e
Selene Huang <selene.huang@gmail.com>
2022-12-03 17:32:24 -0800

    broken setSelected :(

--- src/components/Dropdown/Dropdown.tsx
+++ src/components/Dropdown/Dropdown.tsx
@@ -1,15 +1,8 @@
 // import { Styles } from './styles';
-import React, { useState, useRef, useCallback, useEffect } from 'react';
-import {
-  Dimensions,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  Modal,
-  View,
-  FlatList,
-} from 'react-native';
+import React, { useState, useRef, useCallback } from 'react';
+import { Text, TouchableOpacity, Modal, View, FlatList } from 'react-native';
 import { AntDesign } from '@expo/vector-icons';
+import Styles from './styles';
 import Colors from '../../../assets/Colors';
 
 type DropdownProps = {
@@ -18,104 +11,51 @@ type DropdownProps = {
   onSelect: (item: { label: string; value: string }) => void;
 };
 
-const styles = StyleSheet.create({
-  button: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    backgroundColor: '#efefef',
-    height: 50,
-    width: '90%',
-    paddingHorizontal: 10,
-    zIndex: 1,
-  },
-  buttonText: {
-    flex: 1,
-    textAlign: 'center',
-  },
-  dropdown: {
-    position: 'absolute',
-    backgroundColor: '#fff',
-    width: '100%',
-  },
-  item: {
-    paddingHorizontal: 10,
-    paddingVertical: 10,
-    borderBottomWidth: 1,
-  },
-});
-
 export default function Dropdown({ label, data, onSelect }: DropdownProps) {
   const [visible, setVisible] = useState(false);
-  const { width: W, height: H } = Dimensions.get('window');
-  // const DropdownButton = useRef<TouchableOpacity>(null);
-  const DropdownButton = useRef();
-  // const refList = useRef<FlatList>(null);
+  const DropdownButton = useRef<TouchableOpacity>(null);
   const [dropdownTop, setDropdownTop] = useState(0);
-  const [position, setPosition] = useState({});
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const [selected, setSelected] = useState<any>(null);
+  const [selected, setSelected] = useState<unknown>();
 
   const measure = () => {
-    console.log('hi2');
-    console.log(DropdownButton);
     DropdownButton.current?.measure((_fx, _fy, _w, h, _px, py) => {
       setDropdownTop(py + h);
       // setDropdownTop(500);
     });
   };
-  // DropdownButton.current = 2;
-  // if (DropdownButton?.current) {
-  //   DropdownButton.current.measure((_width, _height, px, py, fx, fy) => {
-  //     const isFull = false;
-  //     const w = Math.floor(px);
-  //     const top = isFull ? 20 : Math.floor(py) + Math.floor(fy) + 2;
-  //     const bottom = H - top;
-  //     const left = Math.floor(fx);
 
-  //     // setDropdownTop(top);
-  //     setDropdownTop(py + H);
-
-  //     console.log(dropdownTop);
-  //     // setDropdownTop(500)
-  // setPosition({
-  //   isFull,
-  //   w,
-  //   top,
-  //   bottom: Math.floor(bottom),
-  //   left,
-  //   height: Math.floor(py),
-  // });
-  // });
-  // }, [H, W]);
-
-  // const openDropdown = (): void => {
-  //   measure();
-  //   setVisible(true);
-  // };
-
-  // const toggleDropdown = (): void => {
-  //   if (visible) {
-  //     setVisible(false);
-  //   } else {
-  //     openDropdown();
-  //   }
-  // };
   const toggleDropdown = () => {
     setVisible(!visible);
   };
 
+  const test = () => {
+    setSelected(8);
+    console.log(selected);
+  };
+
   const onItemPress = (item: { label: string; value: string }): void => {
-    setSelected(item);
-    onSelect(item);
+    setSelected(8);
+    console.log(selected);
+    if (selected?.value === item.value) {
+      console.log('testing');
+      setSelected(null);
+    } else {
+      // console.log(selected?.value);
+      console.log(item.value);
+      // console.log(selected?.value === item.value);
+      setSelected(item);
+      console.log(selected);
+      onSelect(item);
+    }
+    // console.log(selected?.label);
     setVisible(false);
   };
-  // const renderItem = (item: { label: string; value: string }): ReactElement => (
-  // const renderItem = (item: ReactElement) => (
-  // const renderItem = (item: { label: string; value: string }) => (
+
   const renderItem = useCallback(
     // eslint-disable-next-line react/no-unused-prop-types
     ({ item }: { item: { label: string; value: string } }) => (
-      <TouchableOpacity style={styles.item} onPress={() => onItemPress(item)}>
+      <TouchableOpacity style={Styles.item} onPress={() => onItemPress(item)}>
         <Text>{item.label}</Text>
       </TouchableOpacity>
     ),
@@ -125,18 +65,15 @@ export default function Dropdown({ label, data, onSelect }: DropdownProps) {
   const renderDropdown = () => {
     if (visible) {
       measure();
-      console.log('hi!');
       return (
         <Modal visible={visible} transparent animationType="none">
-          {/* <TouchableOpacity style={styles.button} onPress={toggleDropdown}> */}
-          <View style={[styles.dropdown, { top: dropdownTop }]}>
+          <View style={[Styles.dropdown, { top: dropdownTop }]}>
             <FlatList
               data={data}
               renderItem={renderItem}
               keyExtractor={(item, index) => index.toString()}
             />
           </View>
-          {/* </TouchableOpacity> */}
         </Modal>
       );
     }
@@ -145,17 +82,13 @@ export default function Dropdown({ label, data, onSelect }: DropdownProps) {
 
   return (
     <TouchableOpacity
-      style={styles.button}
+      style={Styles.button}
       ref={DropdownButton}
       onPress={toggleDropdown}
     >
       {renderDropdown()}
-      <Text style={styles.buttonText}>{selected?.label || label}</Text>
-      {/* <Text style={styles.buttonText}>Hello</Text> */}
+      <Text style={Styles.buttonText}>{selected?.label || label}</Text>
       <AntDesign name="down" />
     </TouchableOpacity>
   );
-}
-function ref<T>(arg0: null) {
-  throw new Error('Function not implemented.');
 }
--- src/components/Dropdown/styles.ts
+++ src/components/Dropdown/styles.ts
@@ -1,69 +1,29 @@
 import { StyleSheet } from 'react-native';
 
-const styles = StyleSheet.create({
-  mainWrap: {
-    justifyContent: 'center',
+const Styles = StyleSheet.create({
+  button: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#efefef',
+    height: 50,
+    width: '90%',
+    paddingHorizontal: 10,
+    zIndex: 1,
   },
-  container: {
-    flexShrink: 1,
-    borderWidth: 0.5,
-    borderColor: '#EEEEEE',
-    backgroundColor: 'white',
-    shadowColor: '#000',
-    shadowOffset: {
-      width: 0,
-      height: 1,
-    },
-    shadowOpacity: 0.2,
-    shadowRadius: 1.41,
-    elevation: 2,
-  },
-  flex1: {
+  buttonText: {
     flex: 1,
-  },
-  flexShrink: {
-    flexShrink: 1,
-  },
-  wrapTop: {
-    justifyContent: 'flex-end',
+    textAlign: 'center',
   },
   dropdown: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    height: 35,
-  },
-  title: {
-    marginVertical: 5,
-    fontSize: 16,
+    position: 'absolute',
+    backgroundColor: '#fff',
+    width: '100%',
   },
   item: {
-    padding: 17,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-  },
-  textItem: {
-    flex: 1,
-    fontSize: 16,
-  },
-  icon: {
-    width: 20,
-    height: 20,
-  },
-  textError: {
-    color: 'red',
-    fontSize: 14,
-    marginTop: 10,
-  },
-  input: {
-    borderWidth: 0.5,
-    borderColor: '#DDDDDD',
-    paddingHorizontal: 8,
-    marginBottom: 8,
-    margin: 6,
-    height: 45,
+    paddingHorizontal: 10,
+    paddingVertical: 10,
+    borderBottomWidth: 1,
   },
 });
 
-export default styles;
+export default Styles;
