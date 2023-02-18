3f5e0c65f36373fac602402ced93e7b31b953b2c
Ronnie Beggs <ronald.beggs@berkeley.edu>
2023-02-18 15:23:38 -0800

    [feat] create standard logo component

---
+++ src/components/common/StandardLogo.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components/native';
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const v4vLogo = require('../../../assets/logo-1.png');
+
+const LogoContainer = styled.ImageBackground`
+  width: 60px;
+  height: 70px;
+`;
+
+export default function StandardLogo() {
+  return <LogoContainer source={v4vLogo} />;
+}
