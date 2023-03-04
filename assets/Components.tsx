c6ad7960d23fb1ebfcacea7e46f255fb82bd1db4
Allison Hong <allisonhong1023@berkeley.edu>
2023-03-04 15:24:19 -0800

    [minor] frontend fixes

--- assets/Components.tsx
+++ assets/Components.tsx
@@ -74,3 +74,12 @@ export const RootNavBackButton = styled.TouchableOpacity`
   width: 70px;
   padding: 9px;
 `;
+
+// export function NavigationHeaderContainer({ backgroundColor, alignItems });
+
+// NavigationHeaderContainer.propTypes = {
+
+//   backgroundColor: PropTypes.string,
+//   alignItems: PropTypes.string,
+
+// };
