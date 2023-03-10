af3151c82fe8eff88335e2d90e1b6eb63bc84d55
Sauhard Jain <sauhardjain03@gmail.com>
2023-03-10 03:08:01 -0800

    switch out app.json for app.config.js

---
+++ app.config.js
@@ -0,0 +1,48 @@
+import 'dotenv/config';
+
+export default {
+  expo: {
+    name: 'vouchers4veggies',
+    slug: 'vouchers4veggies',
+    owner: 'vouchers4veggies',
+    version: '1.0.0',
+    orientation: 'portrait',
+    icon: './assets/favicon.png',
+    userInterfaceStyle: 'light',
+    // splash: {
+    //   image: './assets/splash.png',
+    //   resizeMode: 'contain',
+    //   backgroundColor: '#ffffff',
+    // },
+    updates: {
+      fallbackToCacheTimeout: 0,
+    },
+    assetBundlePatterns: ['**/*'],
+    ios: {
+      supportsTablet: true,
+      bundleIdentifier: 'org.calblueprint.vouchers4veggies',
+    },
+    android: {
+      package: 'org.calblueprint.vouchers4veggies',
+      //   adaptiveIcon: {
+      //     foregroundImage: './assets/adaptive-icon.png',
+      //     backgroundColor: '#FFFFFF',
+      //   },
+    },
+    web: {
+      favicon: './assets/favicon.png',
+    },
+    extra: {
+      firebaseApiKey: process.env.FIREBASE_API_KEY,
+      firebaseAuthDomain: process.env.FIREBASE_AUTH_DOMAIN,
+      firebaseProjectId: process.env.FIREBASE_PROJECT_ID,
+      firebaseStorageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+      firebaseMessagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+      firebaseAppId: process.env.FIREBASE_APP_ID,
+      firebaseMeasurementId: process.env.FIREBASE_MEASUREMENT_ID,
+      eas: {
+        projectId: 'fc600ca8-9104-4545-93ad-fccb1898ecfe',
+      },
+    },
+  },
+};
