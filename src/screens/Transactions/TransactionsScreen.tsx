84fa3bd86779ff697a6f285ac9795bf9626c394f
Selene Huang <selene.huang@gmail.com>
2023-03-11 14:04:40 -0800

    Added vendor query and implemented authcontext

--- src/screens/Transactions/TransactionsScreen.tsx
+++ src/screens/Transactions/TransactionsScreen.tsx
@@ -3,9 +3,13 @@ import { FlatList } from 'react-native';
 import { H2Heading } from '../../../assets/Fonts';
 import StandardLogo from '../../components/common/StandardLogo';
 import TransactionCard from '../../components/Transactions/TransactionCard';
-import { getTransactionsByVendorUuid } from '../../database/queries';
+import {
+  getTransactionsByVendorUuid,
+  getVendorByEmail,
+} from '../../database/queries';
 import { TransactionStackScreenProps } from '../../navigation/types';
 import { Transaction } from '../../types/types';
+import { useAuthContext } from '../auth/AuthContext';
 import {
   LogoContainer,
   TransactionsContainer,
@@ -18,13 +22,19 @@ export default function TransactionsScreen({
   navigation,
 }: TransactionStackScreenProps<'TransactionsScreen'>) {
   const [transactions, setTransactions] = useState<Transaction[]>([]);
+  const { user } = useAuthContext();
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        // to do: retrieve vendor uuid
-        const transactionsArray = await getTransactionsByVendorUuid('abc');
-        setTransactions(transactionsArray);
+        // todo: retrieve vendor uuid
+        if (user) {
+          const vendor = await getVendorByEmail(user.email);
+          const transactionsArray = await getTransactionsByVendorUuid(
+            vendor.uuid,
+          );
+          setTransactions(transactionsArray);
+        }
       } catch (error) {
         // eslint-disable-next-line no-console
         console.error('(useEffect)[TransactionsScreen]', error);
