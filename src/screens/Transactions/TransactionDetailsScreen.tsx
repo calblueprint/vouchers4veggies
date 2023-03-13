652485cec633dec9180c15c09d5ba01645f4667c
Sauhard Jain <sauhardjain03@gmail.com>
2023-03-13 14:34:53 -0700

    fix: rectify call to LoadingSpinner

--- src/screens/Transactions/TransactionDetailsScreen.tsx
+++ src/screens/Transactions/TransactionDetailsScreen.tsx
@@ -94,7 +94,7 @@ export default function TransactionDetailsScreen({
           </CardContainer>
         </TransactionsContainer>
       ) : (
-        LoadingSpinner()
+        <LoadingSpinner />
       )}
     </TransactionsContainer>
   );
--- src/screens/Transactions/TransactionsScreen.tsx
+++ src/screens/Transactions/TransactionsScreen.tsx
@@ -52,7 +52,7 @@ export default function TransactionsScreen({
       </TitleContainer>
 
       {isLoading ? (
-        LoadingSpinner()
+        <LoadingSpinner />
       ) : (
         <CardContainer>
           <StartOfListView />
