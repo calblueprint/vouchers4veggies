172dbab4487dca171633e55e11e3f0da247b2d95
Ronnie Beggs <ronald.beggs@berkeley.edu>
2023-03-17 12:39:17 -0700

    [refactor] refactor frontend screens to match figma

--- src/screens/scanning/ManualVoucherScreen.tsx
+++ src/screens/scanning/ManualVoucherScreen.tsx
@@ -1,7 +1,5 @@
 import React, { useState } from 'react';
-import CurrencyInput from 'react-native-currency-input';
-import { TextInput } from 'react-native';
-import { ButtonMagenta } from '../../../assets/Components';
+import { ButtonMagenta, ButtonGray } from '../../../assets/Components';
 import Colors from '../../../assets/Colors';
 import Styles from '../../components/InputField/styles';
 import {
@@ -15,7 +13,6 @@ import {
   Header,
   BodyContainer,
   SafeArea,
-  DropDownContainer,
   FieldContainer,
   FormContainer,
 } from './styles';
@@ -28,24 +25,10 @@ export default function ManualVoucherScreen({
   navigation,
 }: ScannerStackScreenProps<'ManualVoucherScreen'>) {
   const [transactionID, setID] = useState<string>('');
-  const [voucherAmount, setVoucherAmount] = useState<number>(0);
-  const [isActive, setIsActive] = useState<boolean>(false);
-
-  // to be used for backend
-  // const [scanCounter, incrementScanned] = useState(0);
-  // const [value, setValue] = useState(null);
-  // const [items, setItems] = useState([
-  //   { label: 'Apple', value: 'apple' },
-  //   { label: 'Banana', value: 'banana' },
-  // ]);
 
   const onChangeSerialNumber = (text: string) => {
     const value = text.replace(/\D/g, '');
     setID(value);
-  };
-
-  const onChangeVoucherAmount = (value: number) => {
-    setVoucherAmount(value ?? 0.0);
   };
 
   const handleVoucherAdd = () => {
@@ -74,38 +57,14 @@ export default function ManualVoucherScreen({
               validate={validateSerialNumberInput}
               keyboardType="number-pad"
             />
-            <DropDownContainer />
-          </FieldContainer>
-          <FieldContainer>
-            <InputTitleText>Amount</InputTitleText>
-            <CurrencyInput // TODO: refactor currency input with custom text input base components
-              value={voucherAmount}
-              onChangeValue={onChangeVoucherAmount}
-              renderTextInput={props => (
-                <TextInput
-                  {...props}
-                  onBlur={() => setIsActive(false)}
-                  onFocus={() => setIsActive(true)}
-                  style={isActive ? Styles.FormFieldFocus : Styles.FormField}
-                  placeholderTextColor={Colors.midGray}
-                  secureTextEntry={false}
-                  autoCorrect={false}
-                  autoCapitalize="none"
-                  keyboardType="number-pad"
-                  returnKeyType="done"
-                />
-              )}
-              prefix="$"
-              minValue={0}
-              maxValue={10}
-              separator="."
-              precision={2}
-            />
           </FieldContainer>
         </FormContainer>
         <ButtonMagenta onPress={handleVoucherAdd}>
           <ButtonTextWhite>Add Voucher</ButtonTextWhite>
         </ButtonMagenta>
+        <ButtonGray>
+          <ButtonTextWhite>Review and Submit</ButtonTextWhite>
+        </ButtonGray>
       </BodyContainer>
     </SafeArea>
   );
