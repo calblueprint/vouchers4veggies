5367988e73824139544d1c65cf650a93b3f9a1bb
Ronnie Beggs <ronald.beggs@berkeley.edu>
2023-03-17 18:36:39 -0700

    [feat] create ScanningContext to track new voucher additions

---
+++ src/screens/scanning/ScanningContext.tsx
@@ -0,0 +1,65 @@
+import React, { createContext, useContext, useMemo, useReducer } from 'react';
+
+export type ScanningDispatch = React.Dispatch<ScanningContextAction>;
+
+type ScanningState = {
+  isEmpty: boolean;
+  voucherMap: Map<number, number>;
+  dispatch: ScanningDispatch;
+};
+
+type ScanningContextAction =
+  | { type: 'ADD_VOUCHER'; serialNumber: number; voucherAmount: number }
+  | { type: 'DELETE_VOUCHER'; serialNumber: number };
+
+const useScanningReducer = () =>
+  useReducer(
+    (prevState: ScanningState, action: ScanningContextAction) => {
+      switch (action.type) {
+        case 'ADD_VOUCHER':
+          return {
+            ...prevState,
+            isEmpty: false,
+          };
+        case 'DELETE_VOUCHER':
+          return {
+            ...prevState,
+          };
+        default:
+          return prevState;
+      }
+    },
+    {
+      isEmpty: true,
+      voucherMap: new Map<number, number>(),
+      dispatch: () => null,
+    },
+  );
+
+const ScanningContext = createContext<ScanningState>({} as ScanningState);
+
+ScanningContext.displayName = 'ScanningContext';
+
+export const useScanningContext = () => useContext(ScanningContext);
+
+export function ScanningContextProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [scanningState, dispatch] = useScanningReducer();
+
+  const ScanningContextValue = useMemo(
+    () => ({
+      ...scanningState,
+      dispatch,
+    }),
+    [scanningState, dispatch],
+  );
+
+  return (
+    <ScanningContext.Provider value={ScanningContextValue}>
+      {children}
+    </ScanningContext.Provider>
+  );
+}
