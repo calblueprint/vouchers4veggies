66084e9d5acbd6eb143278e8db7ae16790814a29
Ronnie Beggs <ronald.beggs@berkeley.edu>
2023-03-22 23:25:46 -0700

    [cleanup] rid isEmpty variable in context

--- src/screens/scanning/ScanningScreen.tsx
+++ src/screens/scanning/ScanningScreen.tsx
@@ -46,7 +46,7 @@ export default function ScanningScreen({
   const [hasPermission, setHasPermission] = useState<boolean>(false);
   const [type] = useState<never>(BarCodeScanner.Constants.Type.back);
   const [scanned, setScanned] = useState<boolean>(true);
-  const { isEmpty, scanCounter } = useScanningContext();
+  const { scanCounter } = useScanningContext();
 
   useEffect(() => {
     const getBarCodeScannerPermissions = async () => {
@@ -129,7 +129,7 @@ export default function ScanningScreen({
         />
       </ScannerContainer>
 
-      {isEmpty ? (
+      {scanCounter === 0 ? (
         <ButtonMagenta disabled={!scanned} onPress={() => setScanned(false)}>
           <ButtonTextWhite>Scan</ButtonTextWhite>
         </ButtonMagenta>
@@ -140,7 +140,7 @@ export default function ScanningScreen({
           </ButtonWhite>
           <ButtonMagenta
             // onPress={() => navigation.navigate('ReviewScreen')}
-            disabled={isEmpty}
+            disabled={scanCounter === 0}
           >
             <ButtonTextWhite>Review & Submit</ButtonTextWhite>
           </ButtonMagenta>
