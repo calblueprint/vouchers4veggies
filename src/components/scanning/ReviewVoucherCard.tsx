8ce095b5729922e27f2c9e084e92f4a8d9afa816
Selene Huang <selene.huang@gmail.com>
2023-03-23 16:17:38 -0700

    Working alerts

--- src/components/scanning/ReviewVoucherCard.tsx
+++ src/components/scanning/ReviewVoucherCard.tsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import Icon from 'react-native-vector-icons/AntDesign';
-import { Alert } from 'react-native';
 import { AntDesign } from '@expo/vector-icons';
 import Colors from '../../../assets/Colors';
 import { Body1Text } from '../../../assets/Fonts';
@@ -15,49 +13,25 @@ import { useScanningContext } from '../../screens/scanning/ScanningContext';
 type ReviewVoucherCardProps = {
   serialNumber: number;
   value: number;
+  showEditDialog: () => void;
+  showDeleteDialog: () => void;
+  setSerialNumber: (serialNumber: number) => void;
 };
 export default function ReviewVoucherCard({
   serialNumber,
   value,
+  showEditDialog,
+  showDeleteDialog,
+  setSerialNumber,
 }: ReviewVoucherCardProps) {
-  const { dispatch } = useScanningContext();
-
-  const onSubmitVoucherAmount = (input: string | undefined) => {
-    try {
-      if (input) {
-        validateVoucherAmount(input);
-        const newValue = parseFloat(input.replace(',', '.')) * 100;
-        editVoucher(dispatch, serialNumber, newValue);
-      }
-    } catch (error) {
-      Alert.alert('Invalid voucher amount.', undefined, [{ text: 'Close' }]);
-    }
-  };
-
   const onEdit = () => {
-    Alert.prompt(
-      'Enter Number',
-      'Edit voucher amount',
-      [{ text: 'Submit', onPress: onSubmitVoucherAmount }],
-      'plain-text',
-      '0',
-      'decimal-pad',
-    );
-  };
-
-  const onDeleteHelper = () => {
-    deleteVoucher(dispatch, serialNumber);
+    showEditDialog();
+    setSerialNumber(serialNumber);
   };
 
   const onDelete = () => {
-    Alert.alert('Are you sure you want to delete this voucher?', undefined, [
-      { text: 'Cancel' },
-      {
-        text: 'Delete',
-        onPress: onDeleteHelper,
-        style: 'destructive',
-      },
-    ]);
+    showDeleteDialog();
+    setSerialNumber(serialNumber);
   };
 
   return (
--- src/screens/scanning/ReviewScreen.tsx
+++ src/screens/scanning/ReviewScreen.tsx
@@ -3,6 +3,7 @@ import { Text, StyleSheet, Alert, FlatList } from 'react-native';
 import { BarCodeScanner, BarCodeScannerResult } from 'expo-barcode-scanner';
 import Icon from 'react-native-vector-icons/AntDesign';
 import Toast from 'react-native-toast-message';
+import Dialog from 'react-native-dialog';
 import {
   ButtonTextBlack,
   Body1Text,
@@ -39,19 +40,88 @@ import { CardContainer } from '../../components/common/styles';
 import { useScanningContext } from './ScanningContext';
 import ReviewVoucherCard from '../../components/scanning/ReviewVoucherCard';
 import { validateVoucherAmount } from '../../utils/validationUtils';
-import { editVoucher } from '../../utils/scanningUtils';
+import { deleteVoucher, editVoucher } from '../../utils/scanningUtils';
 
 export default function ReviewScreen({
   navigation,
 }: ScannerStackScreenProps<'ReviewScreen'>) {
+  const [deleteDialogIsVisible, setDeleteDialogIsVisible] = useState(false);
+  const [editDialogIsVisible, setEditDialogIsVisible] = useState(false);
+  const [editDialogText, setEditDialogText] = useState('0');
+  const [focusedSerialNumber, setFocusedSerialNumber] = useState(0);
   const { voucherMap, dispatch } = useScanningContext();
   const voucherArray = Array.from(voucherMap, ([serialNumber, value]) => ({
     serialNumber,
     value,
   }));
 
+  const setSerialNumber = (serialNumber: number) => {
+    setFocusedSerialNumber(serialNumber);
+  };
+
+  const showEditDialog = () => {
+    setEditDialogIsVisible(true);
+    setEditDialogText('0');
+  };
+
+  const onSubmitVoucherAmount = () => {
+    setEditDialogIsVisible(false);
+    try {
+      if (editDialogText) {
+        validateVoucherAmount(editDialogText);
+        const newValue = Math.round(
+          parseFloat(editDialogText.replace(',', '.')) * 100,
+        );
+        editVoucher(dispatch, focusedSerialNumber, newValue);
+      }
+    } catch (error) {
+      // Alert.alert('Invalid voucher amount.', undefined, [{ text: 'Close' }]);
+    }
+  };
+
+  const showDeleteDialog = () => {
+    setDeleteDialogIsVisible(true);
+  };
+
+  const hideDeleteDialog = () => {
+    setDeleteDialogIsVisible(false);
+  };
+
+  const onDeleteHelper = () => {
+    deleteVoucher(dispatch, focusedSerialNumber);
+    hideDeleteDialog();
+  };
+
   return (
     <SafeArea>
+      {editDialogIsVisible ? (
+        <Dialog.Container visible>
+          <Dialog.Title>Enter Number</Dialog.Title>
+          <Dialog.Description>Edit voucher amount</Dialog.Description>
+          <Dialog.Input
+            placeholderTextColor={Colors.midGray}
+            onChangeText={input => setEditDialogText(input)}
+            secureTextEntry={false}
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="0.00"
+            keyboardType="decimal-pad"
+            returnKeyType="done"
+          />
+          <Dialog.Button label="Submit" onPress={onSubmitVoucherAmount} />
+        </Dialog.Container>
+      ) : null}
+
+      {deleteDialogIsVisible ? (
+        <Dialog.Container visible>
+          <Dialog.Title>
+            Are you sure you want to delete this voucher?
+          </Dialog.Title>
+          <Dialog.Button label="Cancel" onPress={hideDeleteDialog} />
+          <Dialog.Button label="Delete" bold onPress={onDeleteHelper} />
+        </Dialog.Container>
+      ) : null}
+
       <CardContainer>
         <StartOfListView />
         <FlatList
@@ -60,6 +130,9 @@ export default function ReviewScreen({
             <ReviewVoucherCard
               serialNumber={item.serialNumber}
               value={item.value}
+              showEditDialog={showEditDialog}
+              showDeleteDialog={showDeleteDialog}
+              setSerialNumber={setSerialNumber}
             />
           )}
           keyExtractor={item => item.serialNumber.toString()}
