e001a907792beb1d2c71ab8c23efd0a3911ea2c1
Sauhard Jain <sauhardjain03@gmail.com>
2023-04-07 15:59:12 -0700

    feat [wip]: build ProfileScreen

--- src/screens/profile/ProfileScreen.tsx
+++ src/screens/profile/ProfileScreen.tsx
@@ -1,26 +1,52 @@
-import React from 'react';
-import { Button, StyleSheet, View } from 'react-native';
-import { H2Heading, H3Subheading } from '../../../assets/Fonts';
+import React, { useEffect } from 'react';
+import { ButtonTextWhite, H2Heading } from '../../../assets/Fonts';
 import { signOut } from '../../utils/authUtils';
 import { useAuthContext } from '../auth/AuthContext';
+import {
+  ButtonMagenta,
+  CardContainer,
+  SafeArea,
+  StartOfListView,
+} from '../../../assets/Components';
+import StandardHeader from '../../components/common/StandardHeader';
+import StandardLogo from '../../components/common/StandardLogo';
+import { TitleContainer } from './styles';
+import { ProfileStackScreenProps } from '../../navigation/types';
+import { getVendorByEmail } from '../../database/queries';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
-
-export default function ProfileScreen() {
+export default function ProfileScreen({
+  navigation,
+}: ProfileStackScreenProps<'ProfileScreen'>) {
   const { dispatch } = useAuthContext();
 
+  useEffect(user => {
+    const fetchData = async () => {
+      try {
+        const vendor = await getVendorByEmail(user.email);
+        console.log(vendor);
+      } catch (error) {
+        console.log('fetch vendor on profile screen', error);
+      }
+    };
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <H2Heading>Profile Screen</H2Heading>
-      <H3Subheading>To be implemented...</H3Subheading>
-      <Button onPress={() => signOut(dispatch)} title="Sign out" />
-    </View>
+    <SafeArea>
+      <StandardHeader>
+        <StandardLogo />
+      </StandardHeader>
+
+      <TitleContainer>
+        <H2Heading>Profile</H2Heading>
+      </TitleContainer>
+
+      <CardContainer>
+        <StartOfListView />
+      </CardContainer>
+
+      <ButtonMagenta onPress={() => signOut(dispatch)}>
+        <ButtonTextWhite>Sign Out</ButtonTextWhite>
+      </ButtonMagenta>
+    </SafeArea>
   );
 }
---
+++ src/screens/profile/styles.ts
@@ -0,0 +1,37 @@
+import styled from 'styled-components/native';
+
+export const TitleContainer = styled.View`
+  padding-top: 20px;
+  padding-bottom: 20px;
+`;
+
+export const Title = styled.Text`
+  font-family: 'manrope-bold';
+  font-style: normal;
+  font-size: 42px;
+  line-height: 57px;
+  text-align: center;
+`;
+
+export const LeftAlignedContainer = styled.View`
+  width: 100%;
+  justify-content: flex-start;
+  align-items: flex-start;
+  margin-left: 29px;
+  margin-bottom: 13px;
+`;
+
+export const MediumText = styled.Text`
+  font-family: 'manrope-regular';
+  font-style: normal;
+  font-size: 16px;
+  line-height: 22px;
+  text-align: center;
+`;
+
+export const Size14BoldText = styled.Text`
+  font-family: 'manrope-bold';
+  font-style: normal;
+  font-size: 14px;
+  line-height: 19px;
+`;
