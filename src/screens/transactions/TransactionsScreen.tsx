265fa867724c4f996b89b3d252a7e6539e9b8f0d
Selene Huang <selene.huang@gmail.com>
2023-04-08 14:41:17 -0700

    Filtering & sort backend complete

--- src/screens/transactions/TransactionsScreen.tsx
+++ src/screens/transactions/TransactionsScreen.tsx
@@ -6,7 +6,7 @@ import StandardLogo from '../../components/common/StandardLogo';
 import TransactionCard from '../../components/transactions/TransactionCard';
 import { getTransactionsByVendorUuid } from '../../database/queries';
 import { TransactionStackScreenProps } from '../../navigation/types';
-import { Transaction } from '../../types/types';
+import { Transaction, TransactionStatus } from '../../types/types';
 import { useAuthContext } from '../auth/AuthContext';
 import { TitleContainer } from './styles';
 import {
@@ -21,29 +21,65 @@ export default function TransactionsScreen({
 }: TransactionStackScreenProps<'TransactionsScreen'>) {
   const [isLoading, setIsLoading] = useState(true);
   const [isRefreshing, setIsRefreshing] = useState(false);
+  const [defaultTransactions, setDefaultTransactions] = useState<Transaction[]>(
+    [],
+  );
   const [transactions, setTransactions] = useState<Transaction[]>([]);
   const [sort, setSort] = useState('DateDesc');
+  const [filter, setFilter] = useState('');
+  const [filterMin, setFilterMin] = useState(0);
+  const [filterMax, setFilterMax] = useState(0);
   const { vendorUuid } = useAuthContext();
 
+  const filterByDate = () => {
+    const filteredArray = defaultTransactions?.filter(
+      t =>
+        t.timestamp.seconds * 10 ** 6 >= filterMin &&
+        t.timestamp.seconds * 10 ** 6 <= filterMax,
+    );
+    setTransactions(filteredArray);
+  };
+
+  const filterByPaid = () => {
+    const filteredArray = defaultTransactions?.filter(
+      t => t.status === TransactionStatus.PAID,
+    );
+    setTransactions(filteredArray);
+  };
+
+  const filterByUnpaid = () => {
+    const filteredArray = defaultTransactions?.filter(
+      t => t.status === TransactionStatus.UNPAID,
+    );
+    setTransactions(filteredArray);
+  };
+
+  const filterByAmount = () => {
+    const filteredArray = defaultTransactions?.filter(
+      t => t.value >= filterMin && t.value <= filterMax,
+    );
+    setTransactions(filteredArray);
+  };
+
   const sortTransactionsByAmountAsc = () => {
-    const sortedArray = transactions?.sort((a, b) => a.value - b.value);
+    const sortedArray = defaultTransactions?.sort((a, b) => a.value - b.value);
     setTransactions(sortedArray);
   };
 
   const sortTransactionsByAmountDesc = () => {
-    const sortedArray = transactions?.sort((a, b) => b.value - a.value);
+    const sortedArray = defaultTransactions?.sort((a, b) => b.value - a.value);
     setTransactions(sortedArray);
   };
 
   const sortTransactionsByDateAsc = () => {
-    const sortedArray = transactions?.sort(
+    const sortedArray = defaultTransactions?.sort(
       (a, b) => a.timestamp.seconds - b.timestamp.seconds,
     );
     setTransactions(sortedArray);
   };
 
   const sortTransactionsByDateDesc = () => {
-    const sortedArray = transactions?.sort(
+    const sortedArray = defaultTransactions?.sort(
       (a, b) => b.timestamp.seconds - a.timestamp.seconds,
     );
     setTransactions(sortedArray);
@@ -53,8 +89,25 @@ export default function TransactionsScreen({
     try {
       if (Uuid) {
         const transactionsArray = await getTransactionsByVendorUuid(Uuid);
-        setTransactions(transactionsArray);
+        setDefaultTransactions(transactionsArray);
         setIsLoading(false);
+
+        switch (filter) {
+          case 'Date':
+            filterByDate();
+            break;
+          case 'Paid':
+            filterByPaid();
+            break;
+          case 'Unpaid':
+            filterByUnpaid();
+            break;
+          case 'Amount':
+            filterByAmount();
+            break;
+          default:
+            break;
+        }
 
         switch (sort) {
           case 'AmountAsc':
