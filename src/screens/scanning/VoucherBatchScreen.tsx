64e9ae17ae14d23d3c357a6ae7f5fcf87cad7510
Ronnie Beggs <ronald.beggs@berkeley.edu>
2023-04-10 10:15:45 -0700

    [wip][feat] begin creating screen for entering multiple voucher ranges

---
+++ src/screens/scanning/VoucherBatchScreen.tsx
@@ -0,0 +1,195 @@
+import React, { useState } from 'react';
+import Icon from 'react-native-vector-icons/AntDesign';
+import { TouchableOpacity } from 'react-native';
+import Toast from 'react-native-toast-message';
+import {
+  ButtonMagenta,
+  ButtonWhite,
+  AddManuallyButton,
+  SafeArea,
+} from '../../../assets/Components';
+import {
+  ButtonTextWhite,
+  ButtonTextBlack,
+  H4CardNavTab,
+  CenterText,
+  H2Heading,
+  InputTitleText,
+  // CounterText,
+  Body2Subtext,
+} from '../../../assets/Fonts';
+import {
+  TitleContainer,
+  BodyContainer,
+  FieldContainer,
+  FormContainer,
+  // VoucherCounter,
+  ErrorContainer,
+  RedText,
+} from './styles';
+import InputField from '../../components/InputField/InputField';
+import StandardHeader from '../../components/common/StandardHeader';
+import { ScannerStackScreenProps } from '../../navigation/types';
+import Colors from '../../../assets/Colors';
+import { useScanningContext } from './ScanningContext';
+import { serialNumberIsValid } from '../../database/queries';
+import { handlePreventLeave } from '../../utils/scanningUtils';
+
+export default function ManualVoucherScreen({
+  navigation,
+}: ScannerStackScreenProps<'ManualVoucherScreen'>) {
+  const [startSerialNumber, setStartSerialNumber] = useState<string>('');
+  const [startShowInvalidStartError, setStartShowInvalidError] =
+    useState(false);
+  const [startShowDuplicateStartError, setStartShowDuplicateError] =
+    useState(false);
+
+  const [endSerialNumber, setEndSerialNumber] = useState<string>('');
+  const [endShowInvalidError, setEndShowInvalidError] = useState(false);
+  const [endShowDuplicateError, setEndShowDuplicateError] = useState(false);
+
+  const { voucherMap, dispatch } = useScanningContext();
+  const hasUnsavedChanges = Boolean(voucherMap.size);
+
+  const onChangeStartSerialNumber = (text: string) => {
+    const value = text.replace(/\D/g, '');
+    setStartSerialNumber(value);
+  };
+
+  const onChangeEndSerialNumber = (text: string) => {
+    const value = text.replace(/\D/g, '');
+    setEndSerialNumber(value);
+  };
+
+  const handleVoucherAdd = async () => {
+    if (voucherMap.has(Number(startSerialNumber))) {
+      setStartShowDuplicateError(true);
+    } else if (voucherMap.has(Number(endSerialNumber))) {
+      setEndShowDuplicateError(true);
+    } else {
+      const isStartValid = await serialNumberIsValid(Number(startSerialNumber));
+      const isEndValid = await serialNumberIsValid(Number(endSerialNumber));
+
+      // if (isStartValid && isEndValid) {
+      //   const serialNumberInput = Number(serialNumber);
+
+      //   // clears input field if successfully added
+      //   setSerialNumber('');
+      //   setShowInvalidError(false);
+      //   setShowDuplicateError(false);
+
+      //   // TODO: change once we create custom base components for number inputs
+      //   navigation.navigate('ConfirmValueScreen', {
+      //     serialNumber: serialNumberInput,
+      //   });
+      // } else {
+      //   setShowInvalidError(true);
+      // }
+    }
+  };
+
+  return (
+    <SafeArea>
+      <StandardHeader>
+        {/* <TouchableOpacity onPress={() => navigation.navigate('ReviewScreen')}>
+          <VoucherCounter>
+            <CounterText>{voucherMap.size}</CounterText>
+          </VoucherCounter>
+        </TouchableOpacity> */}
+
+        <AddManuallyButton
+          onPress={() => navigation.navigate('ScanningScreen')}
+        >
+          <ButtonTextBlack>
+            <Icon name="scan1" size={14} color={Colors.midBlack} />
+            {'  '}
+            Scan Voucher
+          </ButtonTextBlack>
+        </AddManuallyButton>
+
+        <TouchableOpacity
+          onPress={() =>
+            handlePreventLeave({
+              hasUnsavedChanges,
+              navigation,
+              dispatch,
+            })
+          }
+        >
+          <Icon name="close" size={24} color={Colors.midBlack} />
+        </TouchableOpacity>
+      </StandardHeader>
+
+      <BodyContainer>
+        <TitleContainer>
+          <CenterText>
+            <H2Heading>Add a range of vouchers</H2Heading>
+          </CenterText>
+        </TitleContainer>
+        <FormContainer>
+          <FieldContainer>
+            <InputTitleText>Starting Serial Number</InputTitleText>
+            <InputField
+              onChange={onChangeStartSerialNumber}
+              value={startSerialNumber}
+              placeholder="Enter Number"
+              isValid={!showInvalidError}
+              keyboardType="number-pad"
+            />
+          </FieldContainer>
+          <ErrorContainer>
+            {showInvalidStartError ? (
+              <RedText>
+                <Body2Subtext>Oh no! Invalid serial number.</Body2Subtext>
+              </RedText>
+            ) : null}
+            {showDuplicateStartError ? (
+              <RedText>
+                <Body2Subtext>
+                  You&apos;ve already added this serial number!
+                </Body2Subtext>
+              </RedText>
+            ) : null}
+          </ErrorContainer>
+          <FieldContainer>
+            <InputTitleText>End Serial Number</InputTitleText>
+            <InputField
+              onChange={onChangeEndSerialNumber}
+              value={endSerialNumber}
+              placeholder="Enter Number"
+              isValid={!showInvalidError}
+              keyboardType="number-pad"
+            />
+          </FieldContainer>
+          <ErrorContainer>
+            {showInvalidError ? (
+              <RedText>
+                <Body2Subtext>Oh no! Invalid serial number.</Body2Subtext>
+              </RedText>
+            ) : null}
+            {showDuplicateError ? (
+              <RedText>
+                <Body2Subtext>
+                  You&apos;ve already added this serial number!
+                </Body2Subtext>
+              </RedText>
+            ) : null}
+          </ErrorContainer>
+        </FormContainer>
+
+        <ButtonMagenta disabled={showInvalidError} onPress={handleVoucherAdd}>
+          <ButtonTextWhite>Add Voucher</ButtonTextWhite>
+        </ButtonMagenta>
+        <ButtonWhite
+          onPress={() => navigation.navigate('ReviewScreen')}
+          disabled={voucherMap.size === 0}
+        >
+          <ButtonTextBlack>
+            <H4CardNavTab>Review and Submit</H4CardNavTab>
+          </ButtonTextBlack>
+        </ButtonWhite>
+      </BodyContainer>
+      <Toast />
+    </SafeArea>
+  );
+}
