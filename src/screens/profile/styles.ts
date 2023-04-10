10fc0bd0fd6c86548587a26b2979175e26942d7d
EthanAuyeung <ethana2442@gmail.com>
2023-04-10 14:02:10 -0700

    Profile Screen v1 ready for merge

---
+++ src/screens/profile/styles.ts
@@ -0,0 +1,79 @@
+import styled from 'styled-components/native';
+
+export const HeadingContainer = styled.Text`
+  margin-top: 57px;
+  margin-bottom: 16px;
+  word-break: break-all;
+  width: 100%;
+`;
+export const ButtonInfoContainer = styled.TouchableOpacity`
+  margin-top: 4%;
+  margin-left: 0px;
+`;
+export const ButtonBlank = styled.TouchableOpacity`
+  box-sizing: border-box;
+  height: 0px;
+  width: 110%;
+  border: 0.5px;
+`;
+export const ButtonBottomContainer = styled.TouchableOpacity`
+  margin-top: 3%;
+  margin-left: 0%;
+  width: 100%;
+`;
+export const MagentaButtonContainer = styled.View`
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  margin-top: 180px;
+  width: 100%;
+`;
+export const IconContainer = styled.View`
+  margin-top: 16px;
+  flex-direction: row-reverse;
+  padding-left: 52px;
+`;
+export const EmailHeadingContainer = styled.View`
+  margin-top: 32px;
+  display: flex;
+  align-items: center;
+`;
+export const InputFieldContainer = styled.View`
+  margin-top: 32px;
+  display: flex;
+  align-items: flex-start;
+  margin-bottom: 124px;
+  width: 75%;
+`;
+export const IconBackContainer = styled.View`
+  margin-top: 16px;
+  margin-left: 50px;
+`;
+export const PhoneHeadingContainer = styled.View`
+  margin-top: 30%;
+  margin-left: 15%;
+`;
+export const FormContainer = styled.View`
+  margin-horizontal: 24px;
+  display: flex;
+  align-items: center;
+`;
+export const MainProfileContainer = styled.View`
+  padding-left: 32px;
+  padding-right: 32px;
+  margin-bottom: 64px;
+`;
+export const BackButtonContainer = styled.View`
+  width: 100%;
+  justify-content: flex-start;
+  align-items: flex-start;
+  margin-left: 24px;
+  margin-top: 57px;
+`;
+export const LeftAlignContainer = styled.View`
+  flex: 1;
+  flex-direction: column;
+  justify-content: center;
+  align-content: flex-start;
+  padding-left: 29px;
+`;
