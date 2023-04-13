8d1f8588a107a570d74bb3b3f3cbb607397efe44
Ethan Auyeung <32752381+EthanAuyeung@users.noreply.github.com>
2023-04-13 11:53:36 -0700

    [profile] Implement v1 Profile Screen (screens/profile/ProfileScreen.tsx) (#80)
    * Profile Screen v1 ready for merge
    
    * styling: make styling consistent with designs
    
    * Removed unused styling
    
    ---------
    
    Co-authored-by: Sauhard Jain <sauhardjain03@gmail.com>

--- src/screens/profile/ProfileScreen.tsx
+++ src/screens/profile/ProfileScreen.tsx
@@ -1,26 +1,66 @@
-import React from 'react';
-import { Button, StyleSheet, View } from 'react-native';
-import { H2Heading, H3Subheading } from '../../../assets/Fonts';
+import React, { useEffect, useState } from 'react';
+import {
+  H4CardNavTab,
+  H2Heading,
+  ButtonTextWhite,
+  Body1Text,
+} from '../../../assets/Fonts';
 import { signOut } from '../../utils/authUtils';
 import { useAuthContext } from '../auth/AuthContext';
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+import {
+  HeadingContainer,
+  MagentaButtonContainer,
+  MainProfileContainer,
+  LeftAlignContainer,
+} from './styles';
+import {
+  ButtonMagenta,
+  Row,
+  SafeArea,
+  StartOfListView,
+} from '../../../assets/Components';
+import StandardLogo from '../../components/common/StandardLogo';
+import StandardHeader from '../../components/common/StandardHeader';
+import { getVendor } from '../../database/queries';
 
 export default function ProfileScreen() {
   const { dispatch } = useAuthContext();
-
+  const handleSignOut = async () => signOut(dispatch);
+  const { vendorUuid } = useAuthContext();
+  const [vendorName, setVendorName] = useState<string>(' ');
+  const [vendorEmail, setVendorEmail] = useState<string>(' ');
+  useEffect(() => {
+    const fetchData = async () => {
+      if (vendorUuid) {
+        const vendor = await getVendor(vendorUuid);
+        setVendorName(vendor.name);
+        setVendorEmail(vendor.email);
+      }
+    };
+    fetchData();
+  }, [vendorUuid]);
   return (
-    <View style={styles.container}>
-      <H2Heading>Profile Screen</H2Heading>
-      <H3Subheading>To be implemented...</H3Subheading>
-      <Button onPress={() => signOut(dispatch)} title="Sign out" />
-    </View>
+    <SafeArea>
+      <StandardHeader>
+        <StandardLogo />
+      </StandardHeader>
+      <MainProfileContainer>
+        <HeadingContainer>
+          <H2Heading>Hi, {vendorName}!</H2Heading>
+        </HeadingContainer>
+      </MainProfileContainer>
+      <StartOfListView />
+      <Row>
+        <LeftAlignContainer>
+          <Body1Text>Email</Body1Text>
+          <H4CardNavTab>{vendorEmail}</H4CardNavTab>
+        </LeftAlignContainer>
+      </Row>
+      <MagentaButtonContainer>
+        <ButtonMagenta onPress={handleSignOut}>
+          <ButtonTextWhite>Log Out</ButtonTextWhite>
+        </ButtonMagenta>
+      </MagentaButtonContainer>
+    </SafeArea>
   );
 }
---
+++ src/screens/profile/styles.ts
@@ -0,0 +1,25 @@
+import styled from 'styled-components/native';
+
+export const HeadingContainer = styled.Text`
+  margin-top: 34px;
+  margin-bottom: 31px;
+  text-align: center;
+`;
+export const MagentaButtonContainer = styled.View`
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  margin-top: 180px;
+  width: 100%;
+`;
+export const MainProfileContainer = styled.View`
+  margin-left: 32px;
+  margin-right: 32px;
+`;
+export const LeftAlignContainer = styled.View`
+  flex: 1;
+  flex-direction: column;
+  justify-content: center;
+  align-content: flex-start;
+  padding-left: 29px;
+`;
