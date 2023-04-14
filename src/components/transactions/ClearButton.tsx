5f60c5ffddf56311d56dd4d3d0242eb7a905a37e
Selene Huang <selene.huang@gmail.com>
2023-04-13 23:24:46 -0700

    Sort filter front end complete

--- src/components/transactions/ClearButton.tsx
+++ src/components/transactions/ClearButton.tsx
@@ -4,12 +4,13 @@ import { BlueText, Body1Text, MidGrayText } from '../../../assets/Fonts';
 
 type ClearButtonProps = {
   isDisabled: boolean;
+  onPress: () => void;
 };
-export default function ClearButton({ isDisabled }: ClearButtonProps) {
+export default function ClearButton({ isDisabled, onPress }: ClearButtonProps) {
   if (!isDisabled) {
     return (
       <RightAlignContainer>
-        <ClearButtonContainer>
+        <ClearButtonContainer onPress={onPress}>
           <BlueText>
             <Body1Text>Clear</Body1Text>
           </BlueText>
