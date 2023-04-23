f4ee3a2bf99287a7742bfe00705f9d703189c3d7
EthanAuyeung <ethana2442@gmail.com>
2023-04-23 16:22:24 -0700

    Fixed login formatting

---
+++ src/components/common/StartLogo.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from 'styled-components/native';
+import v4vLogo from '../../../assets/top-bar-logo.png';
+
+const LogoContainer = styled.ImageBackground`
+  width: 90px;
+  height: 100px;
+  margin-bottom: 24px;
+`;
+
+export default function StandardLogo() {
+  return <LogoContainer source={v4vLogo} />;
+}
