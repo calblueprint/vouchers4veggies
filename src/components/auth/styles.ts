0077a31dc59ee782141f2be52bd258ad7e037b72
Selene Huang <selene.huang@gmail.com>
2023-05-03 09:12:17 -0700

    Refactored InputField styling

--- src/components/auth/styles.ts
+++ src/components/auth/styles.ts
@@ -1,40 +1,34 @@
 import { StyleSheet } from 'react-native';
 import Colors from '../../../assets/Colors';
 
-const Styles = StyleSheet.create({
-  FormField: {
+export const styles = StyleSheet.create({
+  fieldDefault: {
     borderWidth: 1,
+    height: 35,
+    paddingVertical: 7,
+    paddingHorizontal: 10,
+    marginTop: '3%',
+    marginBottom: '4%',
+    borderRadius: 5,
     borderColor: Colors.midGray,
     backgroundColor: Colors.lightGray,
-    height: 35,
-    paddingVertical: 7,
-    paddingHorizontal: 10,
-    marginTop: '3%',
-    marginBottom: '4%',
-    borderRadius: 5,
   },
-  FormFieldFocus: {
-    borderWidth: 1,
+  isFocused: {
     borderColor: Colors.magenta,
     backgroundColor: Colors.offWhite,
-    height: 35,
-    paddingVertical: 7,
-    paddingHorizontal: 10,
-    marginTop: '3%',
-    marginBottom: '4%',
-    borderRadius: 5,
   },
-  FormFieldError: {
-    borderWidth: 1,
+  isInvalid: {
     borderColor: Colors.alertRed,
     backgroundColor: Colors.alertLightRed,
-    height: 35,
-    paddingVertical: 7,
-    paddingHorizontal: 10,
-    marginTop: '3%',
-    marginBottom: '4%',
-    borderRadius: 5,
   },
 });
 
-export default Styles;
+export const fieldFocused = StyleSheet.compose(
+  styles.fieldDefault,
+  styles.isFocused,
+);
+
+export const fieldIsInvalid = StyleSheet.compose(
+  styles.fieldDefault,
+  styles.isInvalid,
+);
